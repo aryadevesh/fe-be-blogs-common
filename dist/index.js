13b88840895efe9cca393cfd5d3d91470d7eb059
Devesh Kumar Arya <aryadevesh78@gmail.com>
2024-08-21 18:48:59 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.findBlogInput = exports.updateBlogInput = exports.createBlogInput = exports.signinInput = exports.signupInput = void 0;
+const zod_1 = require("zod");
+exports.signupInput = zod_1.z.object({
+    email: zod_1.z.string().email(),
+    password: zod_1.z.string().min(6),
+    name: zod_1.z.string().optional()
+});
+exports.signinInput = zod_1.z.object({
+    email: zod_1.z.string().email(),
+    password: zod_1.z.string(),
+});
+exports.createBlogInput = zod_1.z.object({
+    title: zod_1.z.string(),
+    content: zod_1.z.string(),
+});
+exports.updateBlogInput = zod_1.z.object({
+    title: zod_1.z.string(),
+    content: zod_1.z.string(),
+    id: zod_1.z.number()
+});
+exports.findBlogInput = zod_1.z.object({
+    id: zod_1.z.number()
+});
